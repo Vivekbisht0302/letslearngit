661fc924f3672b87b570fbf45af814e3ad6d6e9b
Vivek Bisht <bishtvivek0302@gmail.com>
2022-07-31 21:07:32 +0530

    adding edited file of sum.java

---
+++ sum.java
@@ -0,0 +1,11 @@
+/**
+ * sum
+ */
+public class sum {
+
+    public static void main(String[] args) {
+        int a=1;
+        int b=4;
+        System.out.println("sum of a and b is"+ (a+b));
+    }
+}
